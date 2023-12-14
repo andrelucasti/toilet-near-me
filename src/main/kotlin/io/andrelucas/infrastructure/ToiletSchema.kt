1aac6d32ec0eae3c52eca0bd5fefa5b91fe04d45
Andre Lucas <andrelucastic@gmail.com>
2023-12-14 13:09:21 +0000

    First steps with GraphQL

--- src/main/kotlin/io/andrelucas/infrastructure/ToiletSchema.kt
+++ src/main/kotlin/io/andrelucas/infrastructure/ToiletSchema.kt
@@ -3,6 +3,7 @@ package io.andrelucas.infrastructure
 import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
 import io.andrelucas.app.ToiletRequest
 import io.andrelucas.app.ToiletResponse
+import io.andrelucas.business.Coordination
 import io.andrelucas.business.ToiletType
 import java.util.UUID
 
@@ -31,9 +32,28 @@ fun SchemaBuilder.toiletSchema() {
         }
     }
 
-    query("toilet"){
-        resolver { ->
-            "toilet"
+    query("toiletNearMe"){
+        resolver { currentCoordination: Coordination ->
+            listOf(
+                ToiletResponse(
+                    id = UUID.randomUUID().toString(),
+                    name = "Toilet 1",
+                    coordination = Coordination(1.0, 1.0),
+                    type = ToiletType.PUBLIC
+                ),
+                ToiletResponse(
+                    id = UUID.randomUUID().toString(),
+                    name = "Toilet 2",
+                    coordination = Coordination(2.0, 2.0),
+                    type = ToiletType.PUBLIC
+                ),
+                ToiletResponse(
+                    id = UUID.randomUUID().toString(),
+                    name = "Toilet 3",
+                    coordination = Coordination(3.0, 3.0),
+                    type = ToiletType.PUBLIC
+                )
+            )
         }
     }
 }
